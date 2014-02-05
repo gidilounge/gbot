245e2a3cf856fd9c414681c8fdd4bb3d539115db
Gidilounge <gidilounge@Gidilounges-MacBook-Pro.local>
2014-02-05 18:51:37 -0500

    Returning top10 artists

--- app.js
+++ app.js
@@ -3,13 +3,56 @@ var request = require('request');
 
 xmpp.on('online', function() {
     console.log('Yes, I\'m connected!');
+   
+
 });
 
 xmpp.on('chat', function(from, message) {
+
+	var playerObject = function(myobject) {
+		
+		for(i=0; i < myobject.tracks.length; i++)
+	    {
+	     
+	        //console.log('Song ID: %s \nPlays: %s \nTrackname: %s \nArtist: %s \n \n', myobject.tracks[i].id, myobject.tracks[i].plays, myobject.tracks[i].title, myobject.tracks[i].artist.name );
+	  
+	     	//xmpp.send(from, myobject.tracks[i].title);
+	     	xmpp.send(from, myobject.tracks[i].artist.name);   
+
+		}
+
+			console.log(songs);
+	 }
+
+		
+
+		if (message.indexOf('!weather') !== -1) {
+
+			console.log(message.substring(9));
+			xmpp.send(from, message.substring(9));
+
+			//Make API Call
+			request('http://gplayer.herokuapp.com/api/playlist/cloudafrica', function (error, response, body) {
+				
+				if (!error && response.statusCode == 200) {
+				
+					var parsedBody = JSON.parse(body);
+					playerObject(parsedBody);
+				};
+		
+			});
+		}
+		
+		else {	
+			
+			var senderror = 'Sorry, I no understand wetin you mean by ' + message;
+			xmpp.send(from, senderror);
+
+		}
 	
-	xmpp.send(from, 'echo: ' + message);
-    console.log('%s says %s', from, message);
 });
+
+
 
 xmpp.on('error', function(err) {
     console.error(err);
@@ -39,3 +82,6 @@ xmpp.subscribe('olumide.southpaw@gmail.com');
 xmpp.setPresence('available', 'Gidilounge API');
 // check for incoming subscription requests
 xmpp.getRoster();
+
+
+
